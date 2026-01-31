bb1d244a7366a5fa6820218367ddddc402882317
Tanushree Pokhriyal <tpokh2004@gmail.com>
2026-01-31 23:51:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ def root():
         "status": "ok",
         "message": "Honeypot service is running"
     }
+
+# Catch-all so tester never gets 404
+@app.get("/{path:path}")
+def catch_all(path: str):
+    return {
+        "status": "ok",
+        "message": "Honeypot service is running"
+    }
